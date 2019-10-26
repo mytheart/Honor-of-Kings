a56adc740d0997dcf07182b540b7f23ffe0628ff
mytheart <550703617@qq.com>
2019-10-27 00:21:47 +0800

    refactor:完善代码规范

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.set('secret', 'i2u34y12oi3u4y8')
+app.set('secret', 'i2u34y12oi3u4y8');
 
-app.use(require('cors')())
-app.use(express.json())
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use(require('cors')());
+app.use(express.json());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
-require('./plugins/db')(app)
-require('./routes/admin')(app)
-// require('./routes/web')(app)
+require('./plugins/db')(app);
+require('./routes/admin')(app);
+// require('./routes/web')(app);
 
 app.listen(3000, () => {
   console.log('server running in localhost:3000');
