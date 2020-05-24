37547e90322c835d862b1f4ee409e2454a063e08
mytheart <550703617@qq.com>
2020-05-24 16:09:41 +0800

    feat:添加注释

--- server/index.js
+++ server/index.js
@@ -2,15 +2,15 @@ const express = require("express");
 
 const app = express();
 
-app.set('secret', 'i2u34y12oi3u4y8');
+app.set('secret', 'i2u34y12oi3u4y8'); // 密钥应该保存到环境变量里，而不是放到代码里
 
-app.use(require('cors')());
-app.use(express.json());
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(require('cors')()); // 允许跨域
+app.use(express.json()); // 接续传来的json数据
+app.use('/uploads', express.static(__dirname + '/uploads')); // 上传的静态资源的路径
 
-require('./plugins/db')(app);
-require('./routes/admin')(app);
-// require('./routes/web')(app);
+require('./plugins/db')(app); // model层：连接数据库，建表
+require('./routes/admin')(app); // 业务层：后台接口
+// require('./routes/web')(app); // 业务层：前台接口
 
 app.listen(3000, () => {
   console.log('server running in localhost:3000');
