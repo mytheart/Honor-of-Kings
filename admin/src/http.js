245470d8f18988a1daaa2fb558fb9258ead53549
mytheart <550703617@qq.com>
2020-05-24 16:06:22 +0800

    refactor：添加注释

--- admin/src/http.js
+++ admin/src/http.js
@@ -5,20 +5,25 @@ import router from './router'
 const http = axios.create({
   baseURL: 'http://localhost:3000/admin/api'
 })
+
+// 全局的请求拦截器（发送请求前执行）
 http.interceptors.request.use(function (config) {
   // Do something before request is sent
   if (localStorage.token) {
-    config.headers.Authorization = 'Bearer ' + localStorage.token
+    config.headers.Authorization = 'Bearer ' + localStorage.token // +Bearer 是行业规范
   }
   return config;
 }, function (error) {
   // Do something with request error
   return Promise.reject(error);
 });
-http.interceptors.response.use(res => {
+
+// 全局的响应拦截器（接收到请求后进入）
+http.interceptors.response.use(res => { // 200的状态码会进入
   return res
-}, err => {
-  if (err.response.data.message) {
+}, err => { // 前端通用的错误处理，响应拦截
+  if (err.response.data.message) { // 异常的状态码
+    // 弹出一个错误
     Vue.prototype.$message({
       type: 'error',
       message: err.response.data.message
@@ -31,5 +36,6 @@ http.interceptors.response.use(res => {
   
   return Promise.reject(err)
 })
+
 
 export default http
